5f65528a346832002411420c74ad6b6d38246e2c
fatma_turkyilmaz <96384069+FATMATURKYILMAZ@users.noreply.github.com>
2023-02-10 23:25:25 +0300

    Add files via upload

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,31 +1,43 @@
-import nltk 
-from nltk.stem.porter import PorterStemmer
 import numpy as np
+import nltk
+# nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
-Stemmer=PorterStemmer()
 def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
     return nltk.word_tokenize(sentence)
+
+
 def stem(word):
-    return Stemmer.stem(word.lower())
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
 
-def bag_of_words(tokenized_sentence,all_word):
-     tokenized_sentence=[stem(w) for w in tokenized_sentence]
-     bag=np.zeros(len(all_word),dtype=np.float32)
-     for idx,w in enumerate(all_word):
-         if w in tokenized_sentence:
-             bag[idx]=1.0
-     return bag        
 
-sentence=["Hello","How","are","you"]
-word=["hi","hello","I","you","bye","thank","cool"]
-bag=bag_of_words(sentence, word)
-print(bag)
-# =============================================================================
-# a="How lond does shiping day?"
-# print(a)
-# a=tokenize(a)
-# print(a)   
-# =============================================================================
-words=['organized','organize','organize']
-stemmed_word=[stem(w) for w in words]
-print(stemmed_word)
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
+            bag[idx] = 1
+
+    return bag
